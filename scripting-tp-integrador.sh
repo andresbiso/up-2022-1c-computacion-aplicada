927399ce0a51e91a670432c1ea6e78e48306f05a
Lauutt <64222711+lauutt@users.noreply.github.com>
2022-06-20 16:30:55 -0300

    Update scripting-tp-integrador.sh

--- scripting-tp-integrador.sh
+++ scripting-tp-integrador.sh
@@ -23,7 +23,7 @@ palindromo() {
     read CADENA
 
     #Convierto la cadena a lowercase
-    CADENA="$(tr [A-Z] [a-z] <<< "$CADENA")"
+    CADENA="${CADENA,,}"
     
     #Elimino los espacios, las comillas invertidas leen lo que devuelve echo
     CADENA=`echo ${CADENA//[[:blank:]]/}`
