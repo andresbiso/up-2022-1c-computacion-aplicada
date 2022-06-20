d7f9bde87622b2e27208d4382c6d1bf55ceb161e
Andrés Biso <biso.andres@gmail.com>
2022-06-20 17:56:24 -0300

    feat: rename proxy script

--- proxy/script-generador-iptables.sh
+++ proxy/script-generador-iptables.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-cd /etc/network/interfaces.d/
-touch proxy_red
-echo "auto enp0s8" >> proxy_red 
-echo "iface enp0s8 inet static" >> proxy_red
-echo "address 192.168.1.55" >> proxy_red
-echo "netmask 255.255.255.0" >> proxy_red
-echo "gateway 192.168.1.1" >> proxy_red
-
-ifdown enp0s8
-ifup enp0s8
-
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
@@ -33,5 +22,3 @@ iptables -I OUTPUT -s 192.168.1.10 -p tcp --sport 22 -m state --state ESTABLISHE
 iptables -t nat -A POSTROUTING -s 192.168.1.10/32 -d 0.0.0.0/0 -j MASQUERADE
 iptables -A FORWARD -s 192.168.1.10/32 -d 0.0.0.0/0 -i enp0s8 -o enp0s3 -j ACCEPT
 iptables -A FORWARD -d 192.168.1.55/32 -s 0.0.0.0/0 -i enp0s3 -o enp0s8 -j ACCEPT
-
-alias probar_script=scripting-tp-integrador.sh
