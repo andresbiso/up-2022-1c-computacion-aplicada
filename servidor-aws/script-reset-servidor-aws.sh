22bb868c0774a55a9fdd60096f6bf3d6e85ee2fd
Andrés Biso <biso.andres@gmail.com>
2022-06-20 23:51:10 -0300

    Update script-reset-servidor-aws.sh

--- servidor-aws/script-reset-servidor-aws.sh
+++ servidor-aws/script-reset-servidor-aws.sh
@@ -8,3 +8,22 @@ rm /etc/network/interfaces.d/red_servidor_aws
 
 # Levanto placa de red
 ifup enp0s3 
+
+# Borrar usuario profesor
+## Lock usuaurio
+passwd --lock profesor
+## Kill procesos usuario
+killall -9 -u profesor
+## Borrar usuario profesor y su home
+deluser --remove-home profesor
+
+# Borrar ssh-server
+sudo apt purge openssh-server
+rm -rf /etc/ssh
+
+# Borrar postgresql
+sudo apt purge postgresql
+
+# Borrar apache2
+sudo apt purge apache2
+rm -rf /var/www
