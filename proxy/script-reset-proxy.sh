da778a552a70861495b3480a4d2cc3cfa279e560
Andrés Biso <biso.andres@gmail.com>
2022-06-20 22:39:21 -0300

    Update script-reset-proxy.sh

--- proxy/script-reset-proxy.sh
+++ proxy/script-reset-proxy.sh
@@ -1,1 +1,20 @@
 #!/bin/bash
+
+# Bajo placa de red
+ifdown enp0s3 
+
+# Elimino configuración red proxy de interfaces.d
+rm /etc/network/interfaces.d/red_proxy
+
+# Levanto placa de red
+ifup enp0s3 
+
+# Elimino archivos de configuración de cron de la home del root
+rm /root/dia_laboral.cfg
+rm /root/dia_no_laboral.cfg
+
+# Elimino configuración crontab de root
+rm /var/spool/cron/crontabs/root
+
+# Reinicio crond
+systemctl restart cron.service
