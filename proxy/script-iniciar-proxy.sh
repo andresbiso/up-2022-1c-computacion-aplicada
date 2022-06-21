a7f4ed6d04ac8456a6aaaf1d198863e62e2dc3e0
Andrés Biso <biso.andres@gmail.com>
2022-06-21 11:32:05 -0300

    Update script-iniciar-proxy.sh

--- proxy/script-iniciar-proxy.sh
+++ proxy/script-iniciar-proxy.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
+
+# Bajo placas de red
+ifdown enp0s3
+ifdown enp0s8
+
 # Copio configuración red proxy a interfaces.d
 cp red_proxy /etc/network/interfaces.d
+
+# Levanto placas de red
+ifup enp0s3 
+ifup enp0s8
+
 # Copio archivos de configuración a home del root
 cp dia_laboral.cfg /root
 cp dia_no_laboral.cfg /root
+
 # Copio configuración cron a crontab root
 cp crontab_proxy /var/spool/cron/crontabs/root
-
-# Reinicio placa de red
-ifdown enp0s3 
-ifup enp0s3 
 
 # Reinicio crond
 systemctl restart cron.service
