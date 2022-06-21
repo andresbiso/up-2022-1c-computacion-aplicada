d1759356c6e0a2a3ed654f9d416579103a4bad44
Andrés Biso <biso.andres@gmail.com>
2022-06-20 22:11:57 -0300

    Update script-generador-dia-no-laboral.sh

--- proxy/script-generador-dia-no-laboral.sh
+++ proxy/script-generador-dia-no-laboral.sh
@@ -1,4 +1,31 @@
 #!/bin/bash
+
+# Reset General de Iptables
+
+# Vuelvo a aceptar todo el tráfico
+iptables -P INPUT ACCEPT
+iptables -P FORWARD ACCEPT
+iptables -P OUTPUT ACCEPT
+ 
+# Flush de todas las cadenas y reglas
+iptables -F
+ 
+# Borrar todas las cadenas
+iptables -X
+ 
+# Flush de todos los contadores
+iptables -Z 
+
+# Aplico mismo cambios para el resto de las tablas de iptables
+iptables -t nat -F
+iptables -t nat -X
+iptables -t mangle -F
+iptables -t mangle -X
+iptables -t raw -F
+iptables -t raw -X
+
+# Reglas Base
+
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
