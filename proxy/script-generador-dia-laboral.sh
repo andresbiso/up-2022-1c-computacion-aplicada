85fc8f3aa168861be3b5b4b0409925379c84323f
Andrés Biso <biso.andres@gmail.com>
2022-06-20 21:29:04 -0300

    Update and rename script-generador-iptables.sh to script-generador-dia-laboral.sh

--- proxy/script-generador-dia-laboral.sh
+++ proxy/script-generador-dia-laboral.sh
@@ -14,7 +14,9 @@ iptables -A INPUT -p tcp --dport 5432 -m iprange --src-range 192.168.1.0-192.168
 iptables -A INPUT -p tcp --dport 80 -m iprange --src-range 192.168.1.0-192.168.1.255 -j ACCEPT
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -i enp0s8 -o enp0s3 -j ACCEPT
-iptables -A FORWARD -i enp0s3 -o enp0s8 -j ACCEPT  
+iptables -A FORWARD -i enp0s3 -o enp0s8 -j ACCEPT
+
+iptables-save > dia_laboral.cfg
 
 # Dia No Laboral
 iptables -I INPUT -s 192.168.1.10 -p tcp --dport 22 -j ACCEPT
