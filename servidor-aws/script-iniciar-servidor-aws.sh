30ae8fb658830a6181ebb3d53e7a28ebddb426f0
Andrés Biso <biso.andres@gmail.com>
2022-06-21 11:18:56 -0300

    feat: add support for host only adapter server aws

--- servidor-aws/script-iniciar-servidor-aws.sh
+++ servidor-aws/script-iniciar-servidor-aws.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+
+# Bajo placas de red
+ifdown enp0s3
+ifdown enp0s8
+
 # Copio configuración red servidor a interfaces.d
 cp red_servidor_aws /etc/network/interfaces.d
 
-# Reinicio placa de red
-ifdown enp0s3 
+# Levanto placas de red
 ifup enp0s3 
+ifup enp0s8 
