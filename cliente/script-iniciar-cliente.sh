20cfc25ac43e377c93871658eb2d6f34b563bd70
Andrés Biso <biso.andres@gmail.com>
2022-06-21 11:34:43 -0300

    Update script-iniciar-cliente.sh

--- cliente/script-iniciar-cliente.sh
+++ cliente/script-iniciar-cliente.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+
+# Bajo placas de red
+ifdown enp0s3
+ifdown enp0s8
+
 # Copio configuración red cliente a interfaces.d
 cp red_cliente /etc/network/interfaces.d
 
-# Reinicio placa de red
-ifdown enp0s3 
+# Levanto placas de red
 ifup enp0s3 
+ifup enp0s8
+
