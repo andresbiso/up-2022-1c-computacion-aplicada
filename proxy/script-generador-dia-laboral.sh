195ecc40488f6a39c93ed639a3061e0060d0b047
Ignacio Goitea <ignaciogoiteac@gmail.com>
2022-06-21 22:37:33 -0300

    [ADD] reglas en cadena OUTPUT

--- proxy/script-generador-dia-laboral.sh
+++ proxy/script-generador-dia-laboral.sh
@@ -38,7 +38,9 @@ iptables -A INPUT -s 192.168.1.55 -j DROP
 iptables -I INPUT -s 192.168.1.55 -p tcp --dport 22 -j ACCEPT
 iptables -I OUTPUT -d 192.168.1.55 -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport 5432 -m iprange --src-range 192.168.1.0-192.168.1.255 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 5432 -m iprange --dst-range 192.168.1.0-192.168.1.255 -j ACCEPT
 iptables -A INPUT -p tcp --dport 80 -m iprange --src-range 192.168.1.0-192.168.1.255 -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 80 -m iprange --dst-range 192.168.1.0-192.168.1.255 -j ACCEPT
 iptables -t nat -A POSTROUTING -j MASQUERADE
 iptables -A FORWARD -i enp0s8 -o enp0s3 -j ACCEPT
 iptables -A FORWARD -i enp0s3 -o enp0s8 -j ACCEPT
